8897a0abd90e3e76ba52c95ff5d3a49b9cfb87d6
엄지성 <eom071005123@gmail.com>
2023-04-27 23:46:18 +0900

    Java Grammar

---
+++ src/chap_02/_03_Operator3.java
@@ -0,0 +1,19 @@
+package chap_02;
+
+public class _03_Operator3 {
+    public static void main(String[] args) {
+        // 비교 연산자
+        System.out.println(5 > 3); // true
+        System.out.println(5 >= 3) ; // true
+        System.out.println(5 >= 5); // true
+        System.out.println(5 >= 7); // false
+
+        System.out.println(5 < 3); // false
+        System.out.println(5 <= 3); // false
+
+        System.out.println(5 == 5); // true
+        System.out.println(5 == 3); // false
+        System.out.println(5 != 5); // false
+        System.out.println(5 != 3); // true
+    }
+}
