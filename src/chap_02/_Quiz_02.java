811c163c053b3dfe39c72bd4e237a5bc422edec1
엄지성 <eom071005123@gmail.com>
2023-04-29 20:34:36 +0900

    Java grammar

---
+++ src/chap_02/_Quiz_02.java
@@ -0,0 +1,9 @@
+package chap_02;
+
+public class _Quiz_02 {
+    public static void main(String[] args) {
+        int hight = 121;
+        String result = (hight >= 120) ? "탑승 가능합니다." : "탑승 불가능합니다";
+        System.out.println("키가 " + hight + "cm 이므로 " + result);
+    }
+}
---
+++ src/chap_03/_01_String1.java
@@ -0,0 +1,7 @@
+package chap_03;
+
+public class _01_String1 {
+    public static void main(String[] args) {
+
+    }
+}
